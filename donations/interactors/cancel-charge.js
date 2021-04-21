87cae51f789bda305237eeebf019910c408455d1
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-04-21 16:37:51 -0300

    create interactor to cancelcharge

---
+++ donations/interactors/cancel-charge.js
@@ -0,0 +1,21 @@
+const Interactor = require("interactor");
+const { cancel } = require("../utils/request-cancel");
+
+module.exports = class CancelCharge extends Interactor {
+  async run(context) {
+    const result = await this.cancelChargeResult(context);
+
+    if (result.status === 200 || result.status === 201) {
+      return Promise.resolve();
+    }
+    return Promise.reject();
+  }
+
+  cancelChargeResult(context) {
+    this.body = {
+      amount: context.chargeInfo.donatedValue,
+    };
+
+    return cancel(context.chargeId, this.body);
+  }
+};
