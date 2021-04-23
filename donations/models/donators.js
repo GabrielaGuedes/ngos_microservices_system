3b56011d0d48071a40fd90cbb1a5bf0e7c633a2c
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-04-23 12:13:51 -0300

    add timestamps for donators

--- donations/models/donators.js
+++ donations/models/donators.js
@@ -3,57 +3,55 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const { Schema } = mongoose;
 
-const donatorSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  birthDate: {
-    type: Date,
-    required: true,
-  },
-  occupation: {
-    type: String,
-    required: false,
-  },
-  motivation: {
-    type: String,
-    required: false,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    index: {
-      unique: true,
-      dropDups: true,
+const donatorSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    birthDate: {
+      type: Date,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: false,
+    },
+    motivation: {
+      type: String,
+      required: false,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: {
+        unique: true,
+        dropDups: true,
+      },
+    },
+    phone: {
+      type: Number,
+      required: false,
+    },
+    donatedValue: {
+      type: Number,
+      required: false,
     },
   },
-  phone: {
-    type: Number,
-    required: false,
-  },
-  donatedValue: {
-    type: Number,
-    required: false,
-  },
-  lastDonation: {
-    type: Date,
-    required: false,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 donatorSchema.plugin(uniqueValidator);
 
