69b5269f8591d600c2a29fa236c60c1b10e8d460
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-04-24 20:43:23 -0300

    change doneAt to timestamps

--- donations/models/donations.js
+++ donations/models/donations.js
@@ -3,36 +3,35 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const { Schema } = mongoose;
 
-const donationSchema = new Schema({
-  donationId: {
-    type: String,
-    require: true,
-    index: {
-      unique: true,
-      dropDups: true,
+const donationSchema = new Schema(
+  {
+    donationId: {
+      type: String,
+      require: true,
+      index: {
+        unique: true,
+        dropDups: true,
+      },
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    donatorEmail: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: false,
     },
   },
-  status: {
-    type: String,
-    required: true,
-  },
-  doneAt: {
-    type: Date,
-    default: Date.now,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  donatorEmail: {
-    type: String,
-    required: true,
-  },
-  source: {
-    type: String,
-    required: false,
-  },
-});
+  { timestamps: true }
+);
 
 donationSchema.plugin(uniqueValidator);
 
