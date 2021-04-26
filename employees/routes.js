f73ce420585eca5dee3514ed4318eadc37b3e2c0
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-04-26 15:18:19 -0300

    add repo for employees

---
+++ employees/routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const router = express.Router();
+
+// router.use("/api/donators", donators);
+// router.use("/api/donations", donations);
+
+module.exports = router;
