66d3123a4240735013bdba5f91779f1429f4c63b
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-04-30 20:22:59 -0300

    M3 adjusts to migrations work from docker

--- volunteers/config/database.js
+++ volunteers/config/database.js
@@ -11,6 +11,6 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE_NAME,
-  host: "localhost",
+  host: process.env.DB_HOST,
   dialect: "postgres",
 };
