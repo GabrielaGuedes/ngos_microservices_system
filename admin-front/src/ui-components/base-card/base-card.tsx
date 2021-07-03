3ca589c705331eb5e7c508fcd17123022c408fa8
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-07-02 22:54:11 -0300

    add more props to base card

--- admin-front/src/ui-components/base-card/base-card.tsx
+++ admin-front/src/ui-components/base-card/base-card.tsx
@@ -4,11 +4,18 @@ import { BaseCardStyled, CardTitle } from "./base-card.style";
 interface IBaseCard {
   title?: string;
   children?: React.ReactNode;
+  textAlign?: "center" | "left" | "right";
+  style?: {};
 }
 
-const BaseCard: React.FC<IBaseCard> = ({ title, children }) => {
+const BaseCard: React.FC<IBaseCard> = ({
+  title,
+  children,
+  textAlign,
+  style,
+}) => {
   return (
-    <BaseCardStyled>
+    <BaseCardStyled style={{ ...style, textAlign: textAlign }}>
       {title && <CardTitle>{title}</CardTitle>}
       {children}
     </BaseCardStyled>
