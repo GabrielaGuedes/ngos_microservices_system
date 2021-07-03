87e1ea67b99e80c4c18860b86c9dbb60ffad2d87
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-07-03 16:51:29 -0300

    add canceled and finished projects page

--- admin-front/src/components/projects/canceled-projects.tsx
+++ admin-front/src/components/projects/canceled-projects.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProjects } from "../../requests/projects/get-projects";
+import { IProject, IProjectsFilters } from "../../requests/projects/types";
+import { errorToast } from "../../ui-components/toasts/toasts";
+import ProjectsGrid from "./projects-grid";
+import LoadingBox from "../../ui-components/loading-box/loading-box";
+import { cleanEmptyEntries } from "../../utils/empty-entries-cleaner";
+import { Fragment } from "react";
+import DataWithFilters from "../../ui-components/data-with-filters/data-with-filters";
+import FiltersFormFields from "./filters-form-fields";
 
 interface ICanceledProjects {}
 
 const CanceledProjects: React.FC<ICanceledProjects> = () => {
-  return <div>cancelados</div>;
+  const [projectsResult, setProjectsResult] = useState<IProject[]>();
+
+  useEffect(() => {
+    getAllData({ status: "CANCELED" });
+  }, []);
+
+  const refreshData = (filters?: IProjectsFilters): Promise<any> => {
+    return getAllData({ ...filters, status: "CANCELED" });
+  };
+
+  const getAllData = (filters?: IProjectsFilters): Promise<any> => {
+    return getProjects(filters)
+      .then((res) => setProjectsResult(res))
+      .catch(() => errorToast());
+  };
+
+  const container = projectsResult ? (
+    <ProjectsGrid projects={projectsResult} refreshData={refreshData} />
+  ) : (
+    <LoadingBox />
+  );
+
+  const handleConfirmForm = (values: IProjectsFilters): Promise<any> => {
+    const cleanedFilters = cleanEmptyEntries(values, [""]);
+
+    return refreshData(cleanedFilters).then(() => cleanedFilters);
+  };
+
+  return (
+    <Fragment>
+      <DataWithFilters
+        dataContainer={container}
+        filtersFormFields={<FiltersFormFields />}
+        handleConfirmForm={handleConfirmForm}
+      />
+    </Fragment>
+  );
 };
 
 export default CanceledProjects;
--- admin-front/src/components/projects/finished-projects.tsx
+++ admin-front/src/components/projects/finished-projects.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
+import { getProjects } from "../../requests/projects/get-projects";
+import { IProject, IProjectsFilters } from "../../requests/projects/types";
+import { cleanEmptyEntries } from "../../utils/empty-entries-cleaner";
+import DataWithFilters from "../../ui-components/data-with-filters/data-with-filters";
+import FiltersFormFields from "./filters-form-fields";
+import ProjectsGrid from "./projects-grid";
+import { errorToast } from "../../ui-components/toasts/toasts";
+import LoadingBox from "../../ui-components/loading-box/loading-box";
 
 interface IFinishedProjects {}
 
 const FinishedProjects: React.FC<IFinishedProjects> = () => {
-  return <div>finalizados</div>;
+  const [projectsResult, setProjectsResult] = useState<IProject[]>();
+
+  useEffect(() => {
+    getAllData({ status: "FINISHED" });
+  }, []);
+
+  const refreshData = (filters?: IProjectsFilters): Promise<any> => {
+    return getAllData({ ...filters, status: "FINISHED" });
+  };
+
+  const getAllData = (filters?: IProjectsFilters): Promise<any> => {
+    return getProjects(filters)
+      .then((res) => setProjectsResult(res))
+      .catch(() => errorToast());
+  };
+
+  const container = projectsResult ? (
+    <ProjectsGrid projects={projectsResult} refreshData={refreshData} />
+  ) : (
+    <LoadingBox />
+  );
+
+  const handleConfirmForm = (values: IProjectsFilters): Promise<any> => {
+    const cleanedFilters = cleanEmptyEntries(values, [""]);
+
+    return refreshData(cleanedFilters).then(() => cleanedFilters);
+  };
+
+  return (
+    <Fragment>
+      <DataWithFilters
+        dataContainer={container}
+        filtersFormFields={<FiltersFormFields />}
+        handleConfirmForm={handleConfirmForm}
+      />
+    </Fragment>
+  );
 };
 
 export default FinishedProjects;
