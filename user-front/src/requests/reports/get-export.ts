290d737efd109e5a01cd549074683faba31f1608
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-07-17 15:00:46 -0300

    remove unnecessary request

--- user-front/src/requests/reports/get-export.ts
+++
@@ -1,6 +0,0 @@
-import { getRequest } from "../../utils/requests";
-import { REPORTS_ROUTES } from "./routes";
-
-export const getExport = async (): Promise<any> => {
-  return getRequest(`${REPORTS_ROUTES.reports}/export`);
-};
