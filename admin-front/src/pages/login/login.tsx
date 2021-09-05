f91fa3004730cdbcb570d251b744e6de0af293b6
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-09-04 21:26:02 -0300

    add ngo name in login page

--- admin-front/src/pages/login/login.tsx
+++ admin-front/src/pages/login/login.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import LoginForm from "../../components/login/login-form";
 import SignupForm from "../../components/login/signup-form";
 import { canSelfRegister } from "../../requests/authentication/can-self-register";
+import { getDetails } from "../../requests/settings/get-details-config";
 import BaseCard from "../../ui-components/base-card/base-card";
+import { errorToast } from "../../ui-components/toasts/toasts";
 import { Title } from "../../ui-components/typography/typography";
 import { LoginCardContainer, LoginPageContainer } from "./login.style";
 
@@ -10,6 +12,7 @@ interface ILogin {}
 
 const Login: React.FC<ILogin> = () => {
   const [isFirstUser, setIsFirstUser] = useState<boolean>();
+  const [name, setName] = useState<string>();
 
   useEffect(() => {
     canSelfRegister()
@@ -17,11 +20,14 @@ const Login: React.FC<ILogin> = () => {
         setIsFirstUser(requestResult.canSelfRegister);
       })
       .catch(() => setIsFirstUser(false));
+    getDetails()
+      .then((result) => setName(result.name))
+      .catch(() => errorToast());
   }, []);
 
   return (
     <LoginPageContainer>
-      <Title>Nome da ONG</Title>
+      <Title>{name}</Title>
       <LoginCardContainer>
         <BaseCard style={{ minWidth: "350px" }}>
           {isFirstUser ? <SignupForm /> : <LoginForm />}
